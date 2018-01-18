cb6f04da5ea952a726583e143b25c1944d5d3d7b
Sergey Kuzmenko <suntoucha@gmail.com>
2018-01-18 15:14:46 +0300

    Extract id

--- Client.go
+++ Client.go
@@ -7,7 +7,6 @@ import (
 	"errors"
 	"encoding/json"
 	"io/ioutil"
-
 )
 
 
---
+++ ExtractId.go
@@ -0,0 +1,24 @@
+package go_sportradar_football
+
+
+import (
+	"strings"
+	"strconv"
+	"errors"
+)
+
+
+
+func ExtractId(str string) (int, error) {
+	split := strings.Split(str, ":")
+	if len(split) == 0 {
+		return 0, errors.New("Split failed")
+	}
+
+	id, err := strconv.Atoi(split[len(split)-1])
+	if err != nil {
+		return 0, err
+	}
+
+	return id, nil
+}
